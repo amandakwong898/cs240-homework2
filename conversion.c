78785a9dee59c61624ef5a5fcf4f052acddf6dc2
Amanda Kwong <amandakwong898@gmail.com>
2020-02-27 23:09:33 -0500

    first commit

--- conversion.c
+++ conversion.c
@@ -42,41 +42,39 @@ void bin_char(unsigned n, char s[])
 
 /* this function converts the integer num into a q_based string */
 void num2q(int num, char num_q[], int q) {
- 
-    char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int x = 1;
-    int y = 0;
-    while (num > x) {
-        x = x * q;
+    char temp[100];
+    for (int i = 0; num_q[i]; i++) {
+	num_q[i] = '\0';
     } 
-    x /= q;
-    int count = 0;
+    char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; 
     int i = 0;
     while (num > 0) {
-        if (num < x) {
-           num_q[i] = digits[count]; 
-           count = 0;
-           i += 1;
-        }
-        num = num - x;
-        count += 1;
+        int remain = num % q;
+        num = num / q;
+        temp[i++] = digits[remain];
     }
- }
-
+    int k = 0;
+    for (int j = i-1;  j  >= 0; j --){
+	num_q[k] = temp[j];
+	k++;
+    }
+}
 /* this function converts a q_based string into an integer */
 int q2num(char num_q[], int q) {
 
+    for (int i = 0; num_q[i]; i++) {
+	num_q[i] = '\0';
+    }
     char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    //int i = 0; 	
     int num = 0;
     int count = 0;
     while (num_q[count] != '\0') {
         count += 1;
     }
-    for (int i = 0; i < sizeof(num_q); i++) {
-        for (int j = 0; j < sizeof(digits); j++) {
-            if (num_q[i] == digits[j]) {
-                num += pow(q, count - i - 1) * j;
-            }    
+    for (int i = 0; num_q[i]; i++) {
+        for (int j = 0; num_q[j]; j--) {
+            num += pow(q, count - i - 1) * j;    
         }
     }
     return num;
@@ -84,17 +82,14 @@ int q2num(char num_q[], int q) {
 
 
 /* this function converts the float f into a binary string fb[]*/
-/*
 void f2b(float f, char fb[]){
-    int x = f // 1;
-    float f = f - x;
+    int x = (int) f;
+    f  = f - x;
     int i = 0;
     while (x != 0) {
-    	int quotient = x // 2;
-     	int remainder = x % 2;
-     	fb[i] = remainder + '0';
-     	x = quotient;
-     	i++;
+	int remainder = x % 2;
+    	x /= 2;	
+     	fb[i++] = remainder + '0';
     }
     fb[i] = '.';
     i++;
@@ -111,33 +106,24 @@ void f2b(float f, char fb[]){
         }
         fb[i] = integer + '0';
 	i++;
+    }
 }
-*/
 /* this function converts the binary string fb[] into a float */
-/*
-float b2f(char fb[]){
+
+float b2f(char fb[]) {
     int n = 0;
-    for (int i = 0; i < sizeof(fb); i++) {
-        n = fb[sizeof(fb) - 1];
+    int total = 0;
+    for (int i = 0; i < fb[i]; i++) {
+        n = fb[fb[i] - 1];
         total = n / 2;
         i++;
     }
 }
 
-*/
-
 int main(void)
 {
 
     char num_q[100];
-    int  num = 20;
-    // this loop converts num into string num_q and print out  
-    for (int i=2; i<16; i++){
-        num2q(num, num_q, i);
-        print(num_q, i);
-        printf("       num: %d\n\n", q2num(num_q,i));
-    }
-/*    char num_q[100];
     int  num = 2020;
     // this loop converts num into string num_q and print out  
     for (int i=2; i<16; i++){
@@ -145,8 +131,7 @@ int main(void)
         print(num_q, i);
         printf("       num: %d\n\n", q2num(num_q,i));
     }
-
-
+ 
 
     char fb[32];
     // this loop converts f into binary string fb[]       
@@ -163,7 +148,5 @@ int main(void)
     printf("        f: %g\n", f);
     print(fb,2);
     printf("        f: %g\n\n", b2f(fb));
-
-*/
-    
+    return 0;    
 }
